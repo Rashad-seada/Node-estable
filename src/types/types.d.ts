4084ce1d94b46a462f312ad5ed95791c5b69d983
zeyad tamer <zeyadtamer101@gmail.com>
2024-04-07 15:19:38 +0200

    firsit setup

---
+++ src/types/types.d.ts
@@ -0,0 +1,12 @@
+type Children = {
+    readonly children: React.ReactNode
+}
+
+
+type RootTemplateProps = {
+    readonly children: React.ReactNode
+}
+
+type RootLayoutProps = {
+    readonly children: React.ReactNode
+}

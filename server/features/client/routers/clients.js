0f4e7d7712a719541f66ab7961b52fffcfd5b2d3
Fathy2050 <mo.fathy2050r@gmail.com>
2024-04-15 12:26:30 +0200

    fix : changed the get client page number from body to guery parameter

--- server/features/client/routers/clients.js
+++ server/features/client/routers/clients.js
@@ -1,418 +1,84 @@
-const {
-  verifyToken,
-  verifyTokenAndAdmin,
-} = require("../../../core/middleware/verify-token");
-const {
-  Client,
-  clientValidation,
-  pageValidation,
-  updateValidation,
-  updateMembershipValidation,
-} = require("../models/client");
-//import bycrpt
-const bcrypt = require("bcrypt");
-
 //import token
-const jwt = require("jsonwebtoken");
 const express = require("express");
 router = express.Router();
 
-//>>>>>>>>>>>>>>>>>>>> Start Code Here <<<<<<<<<<<<<<<<<<<<<<<<
+const ClientController = require("../controllers/client-controller").ClientController
+
 
 /**
-
  * @desc create New Client
-
  * @route api/Client
-
  * @method Post
-
- * @access public
-
+ * @access private
  */
-router.post("/", async (req, res) => {
-  try {
-    const { error } = clientValidation(req.body);
-    
-    
+router.post(
+  "/",
+   ClientController.createNewClient
+);
 
-    if (error) {
-      res.status(400).json({
-        status_code: -1,
-        message: error.message,
-        error: {
-          message: error.message,
-        },
-      });
-    } else {
-      const client = new Client({
-        username: req.body.username,
-        email: req.body.email,
-        phone: req.body.phone,
-        gender: req.body.gender,
-        age: req.body.age,
-      })
-        .save()
-        .then((docs) => {
-          const { __v, ...other } = docs._doc;
 
-          res.status(200).json({
-            status_code: 1,
-            message: "The client is created successfuly",
-            data: {
-              ...other,
-            },
-          });
-        })
-        .catch((error) => {
-          res.status(500).json({
-            status_code: -2,
-            message:
-              "There was an error when creating the client, please try again",
-            data: null,
-            error: {
-              message: error.message,
-            },
-          });
-        });
-    }
-  } catch (error) {
-    res.status(500).json({
-      status_code: -3,
-      message: "There was a server internal error, please try again",
-      data: null,
-      error: {
-        message: error.message,
-      },
-    });
-  }
-});
 
 /**
  * @desc Get Client by id
  * @route api/Client
  * @method Get
- * @access public
+ * @access private
  */
-router.get("/:id", async (req, res) => {
-  try {
-    Client.findById(req.params.id)
-      .then((docs) => {
-        if (docs) {
-          const { __v, ...other } = docs._doc;
+router.get(
+  "/:id",
+   ClientController.getClientById
+);
 
-          res.status(200).json({
-            status_code: 1,
-            message: "Got the client successfuly",
-            data: {
-              ...other,
-            },
-          });
-        } else {
-          res.status(404).json({
-            status_code: -4,
-            message: "Didnt found the client in our records",
-            data: null,
-            error: {
-              message: "Didnt found the client in our records",
-            },
-          });
-        }
-      })
-      .catch((error) => {
-        res.status(500).json({
-          status_code: -2,
-          message:
-            "There was an error when getting the client, please try again",
-          data: null,
-          error: {
-            message: error.message,
-          },
-        });
-      });
-  } catch (error) {
-    res.status(500).json({
-      status_code: -3,
-      message: "There was a server internal error, please try again",
-      data: null,
-      error: {
-        message: error.message,
-      },
-    });
-  }
-});
+
 
 /**
-
- * @desc Get Client 
-
- * @route api/Client/:id
-
+* @desc Get all Clients
+* @route api/Client/
 * @method Get
-
-* @access public
-
+* @access private
 */
-router.get("/", async (req, res) => {
-  try {
-    const { error } = pageValidation(req.body);
+router.get(
+  "/:page",
+   ClientController.getAllClients
+);
 
-    if (error) {
-      res.status(400).json({
-        status_code: -1,
-        message: error.message,
-        error: {
-          message: error.message,
-        },
-      });
-    } else {
-      // Pagination parameters
-      const pageSize = 10; // Number of documents per page
 
-      // Calculate the number of documents to skip
-      const skip = (req.body.page_number - 1) * pageSize;
-
-      Client.find({})
-        .select("-__v")
-        .skip(skip) // Skip documents
-        .limit(pageSize)
-        .then(async (docs) => {
-          const totalRecords = await Client.countDocuments();
-
-          const maxPages = Math.ceil(totalRecords / pageSize);
-
-          res.status(200).json({
-            status_code: 1,
-            message: "Got the clients successfuly",
-            data: {
-              current_page: req.body.page_number,
-              max_pages: maxPages,
-              client: docs,
-            },
-          });
-        })
-        .catch((error) => {
-          res.status(500).json({
-            status_code: -2,
-            message:
-              "There was an error when getting the client, please try again",
-            data: null,
-            error: {
-              message: error.message,
-            },
-          });
-        });
-    }
-  } catch (error) {
-    res.status(500).json({
-      status_code: -3,
-      message: "There was a server internal error, please try again",
-      data: null,
-      error: {
-        message: error.message,
-      },
-    });
-  }
-});
 
 /**
- * @desc Update Hourse
- * @route api/hourse/:id
+ * @desc Update Client
+ * @route api/client/:id
  * @method patch
- * @access public
+ * @access private
  */
-router.patch("/:id", async (req, res) => {
-  try {
-    const { error } = updateValidation(req.body);
+router.patch(
+  "/:id",
+   ClientController.updateClientById
+);
 
-    if (error) {
-      res.status(400).json({
-        status_code: -1,
-        message: error.message,
-        error: {
-          message: error.message,
-        },
-      });
-    } else {
-      const updateOps = {}; // Object to hold the fields you want to update
 
-      // Example fields to update
-      if (req.body.username) {
-        updateOps.username = req.body.username;
-      }
-      if (req.body.email) {
-        updateOps.email = req.body.email;
-      }
-      if (req.body.age) {
-        updateOps.age = req.body.age;
-      }
-      if (req.body.phone) {
-        updateOps.phone = req.body.phone;
-      }
-
-      Client.findOneAndUpdate(
-        { _id: req.params.id },
-        { $set: updateOps },
-        { new: true }
-      )
-        .select("-__v")
-        .then((docs) => {
-          if (docs) {
-            res.status(200).json({
-              status_code: 1,
-              message: "The clients updated successfuly",
-              data: docs,
-            });
-          } else {
-            res.status(404).json({
-              status_code: -4,
-              message: "Didn't find the client",
-              data: null,
-              error: {
-                message: "Didn't find the client",
-              },
-            });
-          }
-        })
-        .catch((error) => {
-          res.status(500).json({
-            status_code: -2,
-            message:
-              "There was an error when getting the client, please try again",
-            data: null,
-            error: {
-              message: error.message,
-            },
-          });
-        });
-    }
-  } catch (error) {
-    res.status(500).json({
-      status_code: -3,
-      message: "There was a server internal error, please try again",
-      data: null,
-      error: {
-        message: error.message,
-      },
-    });
-  }
-});
 
 /**
- * @desc Delete Hourse
+ * @desc Delete Client
  * @route api/client/:id
  * @method delete
- * @access public
+ * @access private
  */
+router.delete(
+  "/:id",
+   ClientController.deleteClientById
+);
 
-router.delete("/:id", async (req, res) => {
-  try {
-    Client.findByIdAndDelete(req.params.id)
-      .select("-__v")
-      .then((docs) => {
-        if (docs) {
-          res.status(200).json({
-            status_code: 1,
-            message: "The clients deleted successfuly",
-            data: docs,
-          });
-        } else {
-          res.status(404).json({
-            status_code: -4,
-            message: "Didn't find the client",
-            data: null,
-            error: {
-              message: "Didn't find the client",
-            },
-          });
-        }
-      })
-      .catch((error) => {
-        res.status(500).json({
-          status_code: -2,
-          message:
-            "There was an error when getting the client, please try again",
-          data: null,
-          error: {
-            message: error.message,
-          },
-        });
-      });
-  } catch (error) {
-    res.status(500).json({
-      status_code: -3,
-      message: "There was a server internal error, please try again",
-      data: null,
-      error: {
-        message: error.message,
-      },
-    });
-  }
-});
 
-router.patch("/membership-status/:id", async (req, res) => {
-  try {
-    const { error } = updateMembershipValidation(req.body);
 
-    if (error) {
-      res.status(400).json({
-        status_code: -1,
-        message: error.message,
-        error: {
-          message: error.message,
-        },
-      });
-    } else {
-      Client.findOneAndUpdate(
-        { _id: req.params.id },
-        {
-          $set: {
-            membershipStatus: req.body.membership_status,
-          },
-        },
-        { new: true }
-      )
-        .select("-__v")
-        .then((docs) => {
-          if (docs) {
-            res.status(200).json({
-              status_code: 1,
-              message: "The clients updated successfuly",
-              data: docs,
-            });
-          } else {
-            res.status(404).json({
-              status_code: -4,
-              message: "Didn't find the client",
-              data: null,
-              error: {
-                message: "Didn't find the client",
-              },
-            });
-          }
-        })
-        .catch((error) => {
-          res.status(500).json({
-            status_code: -2,
-            message:
-              "There was an error when getting the client, please try again",
-            data: null,
-            error: {
-              message: error.message,
-            },
-          });
-        });
-    }
-  } catch (error) {
-    res.status(500).json({
-      status_code: -3,
-      message: "There was a server internal error, please try again",
-      data: null,
-      error: {
-        message: error.message,
-      },
-    });
-  }
-});
+/**
+ * @desc update Client membership status
+ * @route api/client/:id
+ * @method patch
+ * @access private
+ */
+router.patch(
+  "/membership-status/:id",
+   ClientController.updateClientMembershipStatus
+);
 
 module.exports = router;
